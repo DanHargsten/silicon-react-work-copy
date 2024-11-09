55ce35e84afbe94a187f49d90ffbb42c8b846ff9
Dan Hargsten <dan.hargsten@gmail.com>
2024-11-09 16:35:04 +0100

    backup before API changes to subscibe form

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import './subscribe.scss';
 
 import bellIcon from '../../assets/images/illustrations/bell-icon.svg';
 import envelope from '../../assets/images/icons/envelope.svg';
 
 const Subscribe = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Subscribed with email:", data.email)
+  };
+
+  
   return (
     <section className="subscribe">
       <div className="container">
@@ -15,13 +23,36 @@ const Subscribe = () => {
           <h3 className="subscribe__title">Subscribe to our newsletter</h3>
         </div>
 
-        {/* Email input and button */}
-        <form className="subscribe__form" aria-label="Newsletter subscription form.">
+
+        {/* Email input form */}
+        <form onSubmit={handleSubmit(onSubmit)} noValidate className="subscribe__form" aria-label="Newsletter subscription form.">
           <label htmlFor="email" className="sr-only">Enter your email to subscribe to our newsletter.</label>
-          <div className="subscribe__input-wrapper">
+          
+          <div className="subscribe__input-wrapper">            
+            {/* Envelope icon inside input */}
             <img className="subscribe__input-icon" src={envelope} alt="Envelope icon." />
-            <input className="subscribe__input" id="email" type="email" placeholder="Enter your email" aria-label="Enter your email to subscribe to our newsletter." />
-            <button className="subscribe__button btn-medium btn-primary">Subscribe</button>
+
+            {/* Email input field */}
+            <input
+              className="subscribe__input"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              aria-label="Enter your email to subscribe to our newsletter."
+              {...register("email", {
+                required: "Email is required.",
+                pattern: {
+                  value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+                  message: "Please enter a valid email adress"
+                }
+              })}
+            />
+
+            {/* Submit button */}
+            <button type="submit" className="subscribe__button btn-medium btn-primary">Subscribe</button>
+
+            {/* Error message for email validation */}
+            {errors.email && <span className="subscribe__error">{errors.email.message}</span>}
           </div>
         </form>
 
