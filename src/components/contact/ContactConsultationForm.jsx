50e515290a7ecc108747095e778e63a553bde7b2
Dan Hargsten <dan.hargsten@gmail.com>
2024-11-16 16:08:07 +0100

    backup before creating useContext for API-calls

--- src/components/contact/ContactConsultationForm.jsx
+++ src/components/contact/ContactConsultationForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 
 import './contact-consultation-form.scss';
@@ -12,11 +12,38 @@ const ContactConsultationForm = () => {
       reset,
    } = useForm();
 
-   const onSubmit = (data) => {
-      console.log('Form submitted: ', data);
-      alert('Appointment submitted successfully!');  // temporary
+   const [submitted, setSubmitted] = useState(false);
 
-      reset();
+      
+   const onSubmit = async (data) => {
+      console.log('Form submitted: ', data);
+      try {
+         const response = await fetch('https://win24-assignment.azurewebsites.net/api/forms/contact', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+               fullName: data.fullName,
+               email: data.email,
+               specialist: data.specialist,
+            }),
+         });
+
+         if (response.ok) {
+            console.log('Contact form submitted successfully')
+            setSubmitted(true);
+            reset();         
+         } else {
+            console.error('Failed to submit contact form');
+         }
+      } catch (error) {
+         console.error('Error:', error);
+      }
+   };
+
+   const handleOk = () => {
+      setSubmitted(false);
    };
 
 
@@ -25,85 +52,98 @@ const ContactConsultationForm = () => {
       <div className="consultation">
          <h3 className="consultation__header">Get Online Consultation</h3>
 
+         {/* Show validation message when the form is successfully sent */}
+         {submitted ? (
+            <div className="info-overlay">
+               <div className="info-box">
+                  <h1>Thank you for your message</h1>
+                  <p>We'll return to you as soon as we can</p>
+                  <button className="btn-small btn-primary" onClick={handleOk}>Okay</button>
+               </div>
+            </div>
+         ) : (
       
-      <form
-         className="consultation__form"
-         onSubmit={async (e) => {
-            e.preventDefault();
-            const result = await trigger();
-            if (result) handleSubmit(onSubmit)(e);
-         }}
-      >
+         <form
+            className="consultation__form"
+            onSubmit={async (e) => {
+               e.preventDefault();
+               const result = await trigger();
+               if (result) handleSubmit(onSubmit)(e);
+            }}
+         >
          
-         {/* Full name */}
-         <div>
-            <label className="consultation__label" htmlFor="full-name">Full name</label>
-            <input
-            className="consultation__input"
-            id="full-name"
-            {...register('fullName', {
-               required: 'Full name is required',
-               pattern: {
-                  value: /^[A-Za-z]+( [A-Za-z]+)+$/,
-                  message: 'Please enter your full name with at least two words',
-               },
-             })}
-            type="text"
-         />
-         {errors.fullName && (
-            <p className="contact-error">{errors.fullName.message}</p>
-         )}
-         </div>
-
-        
-         {/* Email Adress */}
-         <div>
-            <label className="consultation__label" htmlFor="email">Email adress</label>
-            <input
+            {/* Full name */}
+            <div>
+               <label className="consultation__label" htmlFor="full-name">Full name</label>
+               <input
                className="consultation__input"
-               id="email"
-               {...register('email', {
-                  required: 'Email is required',
+               id="full-name"
+               placeholder="Enter your full name"
+               {...register('fullName', {
+                  required: 'Full name is required',
                   pattern: {
-                     value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                     message: 'Please enter a valid email adress'
+                     value: /^[A-Za-z]+( [A-Za-z]+)+$/,
+                     message: 'Please enter your full name with at least two words',
                   },
                })}
                type="text"
             />
-               {errors.email && (
-                  <p className="contact-error">{errors.email.message}</p>
-               )}          
+            {errors.fullName && (
+               <p className="contact-error">{errors.fullName.message}</p>
+            )}
             </div>
 
-        
-         {/* Specialist Selection */}  
-         <div className="consultation__select">
-            <label className="consultation__label" htmlFor="select">Specialist</label>
-            <select
-               className="consultation__input"
-               id="select"
-               {...register('specialist', {
-                  required: 'Please select a specialist',
-                  validate: (value) =>
-                     value !== 'default' || 'Please select a specialist',
-               })}
-            >
-               <option value="default" >Select a Specialist</option>
-               <option value="specialist1">Specialist 1</option>
-               <option value="specialist2">Specialist 2</option>
-               <option value="specialist3">Specialist 3</option>
-               <option value="specialist4">Specialist 4</option>
-               <option value="specialist5">Specialist 5</option>
-            </select>
-            {errors.specialist && (
-               <p className="contact-error">{errors.specialist.message}</p>
-            )}
-         </div>
+         
+            {/* Email Adress */}
+            <div>
+               <label className="consultation__label" htmlFor="email">Email adress</label>
+               <input
+                  className="consultation__input"
+                  id="email"
+                  placeholder="Enter your email"
+                  {...register('email', {
+                     required: 'Email is required',
+                     pattern: {
+                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                        message: 'Please enter a valid email adress'
+                     },
+                  })}
+                  type="text"
+               />
+                  {errors.email && (
+                     <p className="contact-error">{errors.email.message}</p>
+                  )}          
+               </div>
 
-         {/* Submit Button */}
-         <button className="consultation__button" type="submit" aria-label="Make an appointment">Make an appointment</button>  
-      </form>
+         
+            {/* Specialist Selection */}  
+            <div className="consultation__select">
+               <label className="consultation__label" htmlFor="select">Specialist</label>
+               <select
+                  className="consultation__input"
+                  id="select"
+                  {...register('specialist', {
+                     required: 'Please select a specialist',
+                     validate: (value) =>
+                        value !== 'default' || 'Please select a specialist',
+                  })}
+               >
+                  <option value="default" >Select a Specialist</option>
+                  <option value="specialist1">Specialist 1</option>
+                  <option value="specialist2">Specialist 2</option>
+                  <option value="specialist3">Specialist 3</option>
+                  <option value="specialist4">Specialist 4</option>
+                  <option value="specialist5">Specialist 5</option>
+               </select>
+               {errors.specialist && (
+                  <p className="contact-error">{errors.specialist.message}</p>
+               )}
+            </div>
+
+            {/* Submit Button */}
+            <button className="consultation__button" type="submit" aria-label="Make an appointment">Make an appointment</button>  
+         </form>
+      )}
    </div>
   );
 };
--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import './subscribe.scss';
@@ -6,69 +6,107 @@ import './subscribe.scss';
 import bellIcon from '../../assets/images/illustrations/bell-icon.svg';
 import envelope from '../../assets/images/icons/envelope.svg';
 
+
 const Subscribe = () => {
-  const { register, handleSubmit, formState: { errors }, clearErrors } = useForm();
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-  const onSubmit = (data) => {
-    console.log("Subscribed with email:", data.email)
-  };
+  const [submitted, setSubmitted] = useState(false);
 
-  const handleInputChange = () => {
-    if (errors.email) {
-      clearErrors("email");
+  const onSubmit = async (data) => {
+    console.log("Subscribed with email:", data.email);
+    try {
+      const response = await fetch('https://win24-assignment.azurewebsites.net/api/forms/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: data.email,
+        }),
+    });
+
+    if (response.ok) {
+      console.log('Subscription successful');
+      setSubmitted(true);
+      reset();
+    } else {
+      console.error('Failed to subscribe')
     }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+  const handleOk = () => {
+    setSubmitted(false);
   };
 
   
 
   
-  return (
-    <section className="subscribe">
-      <div className="container">
+return (
+  <section className="subscribe">
+    <div className="container">
 
-        {/* Bell-icon and header */}
-        <div className="subscribe__header">
-          <img className="subscribe__bell-icon" src={bellIcon} alt="Subscription bell image." />
-          <h3 className="subscribe__title">Subscribe to our newsletter</h3>
-        </div>
-
-
-        {/* Email input form */}
-        <form onSubmit={handleSubmit(onSubmit)} noValidate className="subscribe__form" aria-label="Newsletter subscription form.">
-          <label htmlFor="email" className="sr-only">Enter your email to subscribe to our newsletter.</label>
+      {submitted && (
+        <>
+          <div className="info-overlay">
+            <div className="info-box">
+              <h1>Thank you for subscribing</h1>
+              <p>Stay put for our newsletters</p>
+              <button className="btn-small btn-primary" onClick={handleOk}>Okay</button>
+            </div>
+          </div>
+        </>
+      )}
           
-          <div className="subscribe__input-wrapper">            
-            {/* Envelope icon inside input */}
-            <img className="subscribe__input-icon" src={envelope} alt="Envelope icon." />
+      {/* Bell-icon and header */}
+      <div className="subscribe__header">
+        <img className="subscribe__bell-icon" src={bellIcon} alt="" aria-hidden="true" />
+        <h3 className="subscribe__title">Subscribe to our newsletter</h3>
+      </div>
 
-            {/* Email input field */}
-            <input
-              className="subscribe__input"
-              id="email"
-              type="email"
-              placeholder="Enter your email"
-              aria-label="Enter your email to subscribe to our newsletter."
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
-                  message: "Please enter a valid email adress"
-                }
-              })}
-              onInput={handleInputChange}
-            />
+
+      {/* Email input form */}
+      <form onSubmit={handleSubmit(onSubmit)} noValidate className="subscribe__form" aria-label="Newsletter subscription form.">
+        <label htmlFor="email" className="sr-only">Enter your email to subscribe to our newsletter.</label>
+      
+        <div className="subscribe__input-wrapper">            
+          {/* Envelope icon inside input */}
+          <img className="subscribe__input-icon" src={envelope} alt="Envelope icon." />
+
+          {/* Email input field */}
+          <input
+            className="subscribe__input"
+            id="email"      
+            placeholder="Enter your email"
+            aria-label="Enter your email to subscribe to our newsletter."
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+                message: "Please enter a valid email adress"
+              },
+            })}
+            type='text'
+          />
+            {errors.email && (
+              <p className="subscribe-error">{errors.email.message}</p>
+            )}
 
             {/* Submit button */}
-            <button type="submit" className="subscribe__button btn-medium btn-primary">Subscribe</button>
+            <button type="submit" className="subscribe__button btn-medium btn-primary">Subscribe</button>  
+        </div>
+      </form>    
+    </div>
+  </section> 
+  );
+};
 
-            {/* Error message for email validation */}
-            {errors.email && <span className="subscribe__error">{errors.email.message}</span>}
-          </div>
-        </form>
-
-      </div>
-    </section>
-  )
-}
-
-export default Subscribe
+export default Subscribe;
